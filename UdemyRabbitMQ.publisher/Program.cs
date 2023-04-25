346141649e8c4b56028ac93ed987c98dea91018c
Hüseyin Baysal <hsyn_bysl@hotmail.com>
2023-04-25 10:52:40 +0300

    Add project files.

---
+++ UdemyRabbitMQ.publisher/Program.cs
@@ -0,0 +1,49 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace UdemyRabbitMQ.publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //RabbitMQ ile bağlantı kurulmalı
+            //Connection factori oluşturulur
+            var factory = new ConnectionFactory();
+            //Gerçek uygulamalarda bu Uri AppSettingJson içinde tutulur
+            factory.Uri = new Uri("amqps://mdqxnrpk:bKVsEpaaMzJNGAvfuXKvfvFL4nzVNEL_@toad.rmq.cloudamqp.com/mdqxnrpk");
+
+            using var connection = factory.CreateConnection();
+            //Bir kanal oluşturulur.
+            var channel = connection.CreateModel();
+            //RabbitMQ ile kanal üzerinden haberleşilir.
+            //Channel üzerinden birde kuyruk oluşturulur.
+
+            channel.QueueDeclare("hello-queue", true, false, false);
+
+            Enumerable.Range(1, 50).ToList().ForEach(x => 
+            {
+                string message = $"Mesaj {x}";
+
+                //Bütün veriler byte dizisine çevrilir.
+                var messageBody = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(string.Empty, "hello-queue", null, messageBody);
+
+                Console.WriteLine($"Mesaj Gönderilmiştir : {message}");
+            });
+
+            //Queue methodu içindeki durable property'si, durable false olursa, rabbitmq de oluşan kuyruklar Memory'de tutulur, eğer rabbitmq restrtlanırsa tüm kuyruk gidilir. Eğer durable true olursa, rabbitmq de oluşan kuyruklar storage'de tutulur rabbitmq restartlanırsa bile gitmez ve sırayla devam eder.
+
+            //exclusive property'si, bu property true olursa, bu kuyruğa sadece bu kanaldan erişilebilir. eğer false olursa subscribe tarafında başka bir kanaldan erişilebilir.
+
+            //autoDelete property'si bu property true olursa rabbitmq down olursa kuyruktaki herşey silinir. false olursa rabbitmq down olursa kuyruk silinmez yine de beklemeye devam eder.
+
+            
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ UdemyRabbitMQ.subscriber/Program.cs
@@ -0,0 +1,49 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace UdemyRabbitMQ.subscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://mdqxnrpk:bKVsEpaaMzJNGAvfuXKvfvFL4nzVNEL_@toad.rmq.cloudamqp.com/mdqxnrpk");
+
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+            //channel.QueueDeclare("hello-queue", true, false, false);
+
+            channel.BasicQos(0, 1,false);
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            channel.BasicConsume("hello-queue",false,consumer);
+
+
+
+            //consumer.Received += Consumer_Received;
+            consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+            {
+                var message = Encoding.UTF8.GetString(e.Body.ToArray());
+                Thread.Sleep(1500);
+                Console.WriteLine("Gelen Mesaj : " + message);
+                //BasicAckno
+                channel.BasicAck(e.DeliveryTag,false);
+            };
+
+
+            Console.ReadKey();
+        }
+
+        //private static void Consumer_Received(object sender, BasicDeliverEventArgs e)
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+}
